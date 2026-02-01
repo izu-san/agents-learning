a03b9b44a646087055dada0f3f2620d187b86e8c
izu-san <s.muro.1214@gmail.com>
2026-02-01 15:12:56 +0900

    Healerお試し結果を追加

--- tests/standard/data/plans.ts
+++ tests/standard/data/plans.ts
@@ -2,7 +2,7 @@ export const plans = {
   recommended: {
     id: 0,
     name: 'お得な特典付きプラン',
-    basePrice: 7000,
+    basePrice: 10500, // NOTE: 宿泊プラン料金が1.5倍に変更されたためテストデータを更新
     weekendMultiplier: 1.25,
     minPeople: 1,
     maxPeople: 9,
